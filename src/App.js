40fde27852cc1f3f6a5f23e077932718e1aca77a
Oleg Kireev <okireev@ubic.tech>
2022-04-05 18:46:58 +0300

    @/App: add Passengers

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import Passengers from "./features/Passengers";
+
 function App() {
   return (
     <div className="App">
       <h1>Hello React-query</h1>
+      <Passengers />
     </div>
   );
 }
