7639a99e6d0af0e48c704847585e62d516fed155
Oleg Kireev <okireev@ubic.tech>
2022-04-05 18:45:13 +0300

    @/index: wrap App with QueryClientProvider

--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@ import React from 'react';
 import { createRoot } from 'react-dom/client'
 import './index.css';
 import App from './App';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root'))
   .render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </QueryClientProvider>
   )
