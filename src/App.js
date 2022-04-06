849b898599f903d1a84cdabf3c980aebdcd22a6d
Oleg Kireev <okireev@ubic.tech>
2022-04-06 11:20:18 +0300

    @/App: use PasengerFinder

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Passengers from "./features/Passengers";
+import PassengerFinder from "./features/PassengerFinder";
 
 function App() {
   return (
     <div className="container">
       <h1>Hello React-query</h1>
       <Passengers />
+      <PassengerFinder />
     </div>
   );
 }
